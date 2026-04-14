cffdf67bea87e9b8ad65ae62fa0706a964f96360
Lennox Lewis <lennylewis6a6y@gmail.com>
2026-04-14 13:56:25 +0300

    3D render issue fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,7 +27,7 @@ const words = [
   { text: "Designs", imgPath: "/images/designs.svg" },
   { text: "Code", imgPath: "/images/code.svg" },
   { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Concept", imgPath: "/images/concepts.svg" },
   { text: "Designs", imgPath: "/images/designs.svg" },
   { text: "Code", imgPath: "/images/code.svg" },
 ];
