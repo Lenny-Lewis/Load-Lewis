28461b129ba4c5b8acbc3ca429e0ead7b996fd5a
Lennox <lennylewis6a6y@gmail.com>
2026-09-17 22:02:50 +0300

    ZedOS project template updated

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -50,7 +50,7 @@ const webProjects = [
     id: 6,
     title: "Zedos Technologies",
     desc: "Corporate website and digital solutions platform.",
-    img: "/images/zedos_hero_template.png", 
+    img: "/images/ZedOS_Template.webp", 
     link: "http://zedostechnologies.co.ke/",
     bg: "bg-[#FFE7EB]"
   },
